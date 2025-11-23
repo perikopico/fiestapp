e77609c873625363b0003d885c4e93b22dbc12f3
Pedro Muñoz Alba <pedromunozalba@gmail.com>
2025-11-23 22:14:37 +0100

    fix: integración maps + cleanup

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,23 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    // Forzar Java 11 en todos los subproyectos (incluyendo plugins) para evitar warnings de Java 8
+    afterEvaluate {
+        project.extensions.findByType<com.android.build.gradle.BaseExtension>()?.apply {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+        }
+        
+        // Forzar JVM 11 para Kotlin en todos los subproyectos
+        project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_11.toString()
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
