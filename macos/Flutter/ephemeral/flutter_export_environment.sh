5751c829c69dc438ada784c86137fb73b22e03ef
Pedro Muñoz Alba <pedromunozalba@gmail.com>
2026-01-16 22:30:07 +0100

    mejoras json

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/perikopico/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/perikopico/StudioProjects/fiestapp"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\pedro\Projects\fiestapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.1.0"
